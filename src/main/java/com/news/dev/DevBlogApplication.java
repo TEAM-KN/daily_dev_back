ea200f98f278aaa7541bc65aa23a87539dfc018a
s_chuli <stc9606@naver.com>
2022-04-30 17:14:48 +0900

    feat: project naming modify

--- src/main/java/com/news/dev/DevBlogApplication.java
+++ src/main/java/com/news/dev/DevBlogApplication.java
@@ -1,18 +1,15 @@
 package com.news.dev;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class DevNewsApplication {
+public class DevBlogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DevNewsApplication.class, args);
+        SpringApplication.run(DevBlogApplication.class, args);
     }
 
     @Bean
