7080b9963565de0ed0a2961944f12bfad1c8b709
s_chuli <stc9606@naver.com>
2022-05-09 16:10:09 +0900

    feat: Contents Batch Job 구현

--- src/main/java/com/news/dev/DevBlogApplication.java
+++ src/main/java/com/news/dev/DevBlogApplication.java
@@ -1,11 +1,14 @@
 package com.news.dev;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableScheduling
 public class DevBlogApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/news/dev/adaptor/WoowahanAdaptor.java
+++ src/main/java/com/news/dev/adaptor/WoowahanAdaptor.java
@@ -92,21 +92,13 @@ public class WoowahanAdaptor {
 
         return contents;
     }
+    
+    // New Contents Checking
+    public List<ContentsEntity> getNewContents() {
 
-    // New Contents Update (Batch)
-    public List<ContentsEntity> newContentsUpdate() {
         Document doc = getDocument();
         Elements elements = getElement(doc);
         List<ContentsDto> contents = setContents(elements);
-        List<ContentsEntity> newContents = getNewContents(contents);
-
-        List<ContentsEntity> rsEntity = contentsRepository.saveAll(newContents);
-        return rsEntity;
-    }
-
-    
-    // New Contents Checking
-    public List<ContentsEntity> getNewContents(List<ContentsDto> contents) {
 
         List<ContentsDto> newContents = new ArrayList<>();
         LocalDate nowDtm = LocalDate.now();
@@ -124,6 +116,14 @@ public class WoowahanAdaptor {
         return contents.stream().map(content ->
             new ModelMapper().map(content, ContentsEntity.class)
         ).collect(Collectors.toList());
+    }
+
+
+    // New Contents Update (Batch)
+    public List<ContentsEntity> newContentsUpdate() {
+        List<ContentsEntity> newContents = getNewContents();
+        List<ContentsEntity> rsEntity = contentsRepository.saveAll(newContents);
+        return rsEntity;
     }
 }
 
