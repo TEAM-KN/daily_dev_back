9b7e1cbbab48a96fd980a5cd867c6be55e74161a
s_chuli <stc9606@naver.com>
2022-07-24 23:58:44 +0900

    batch program test code 작성

--- src/test/java/com/news/dev/batch/ContentsConfigurationTest.java
+++ src/test/java/com/news/dev/batch/ContentsConfigurationTest.java
@@ -1,25 +1,31 @@
 package com.news.dev.batch;
 
+import com.news.dev.TestConfiguration;
 import com.news.dev.config.batch.contents.ContentsConfiguration;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@SpringBatchTest
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = { TestConfiguration.class, ContentsConfiguration.class })
 public class ContentsConfigurationTests {
 
     @Autowired
     private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private ContentsConfiguration contentsConfiguration;
 
     @Test
     public void test_get_contents() throws Exception {
@@ -29,10 +35,14 @@ public class ContentsConfigurationTests {
                 .toJobParameters();
 
         // when
+        jobLauncherTestUtils.setJob(contentsConfiguration.contentsJob());
         JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
 
         // then
-//        Assertions.assertThat(jobExecution.getStepExecutions().stream());
-
+        Assertions.assertThat(jobExecution.getStepExecutions()
+                .stream()
+                .mapToInt(StepExecution::getWriteCount)
+                .sum()
+        ).isNotEqualTo(0);
     }
 }
