c8a15614051ebc94dfdb2ac8fee6682bcdb446ae
stc9606 <stc9606@naver.com>
2023-03-13 23:40:34 +0900

    fix. token 추출 header key 변경

--- src/main/java/com/daily/auth/application/AuthExtractor.java
+++ src/main/java/com/daily/auth/application/AuthExtractor.java
@@ -4,8 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
 
 public class AuthExtractor {
-    private AuthExtractor() {
-    }
+    private AuthExtractor() {}
 
     public static final String AUTH_TYPE = "Bearer";
     public static final String AUTH_HEADER_KEY = "Authorization";
@@ -14,7 +13,7 @@ public class AuthExtractor {
         Enumeration<String> headers = request.getHeaders(AUTH_HEADER_KEY);
         while (headers.hasMoreElements()) {
             String value = headers.nextElement();
-            if (value.toLowerCase().startsWith(AUTH_HEADER_KEY.toLowerCase())) {
+            if (value.toLowerCase().startsWith(AUTH_TYPE.toLowerCase())) {
                 return value.split(" ")[1];
             }
         }
