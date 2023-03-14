abd5e6f1358061942decb773d84a613b876ba8f2
stc9606 <stc9606@naver.com>
2023-03-14 23:41:26 +0900

    test. contents Job 테스트 코드 작성

--- src/test/java/com/daily/TestConfiguration.java
+++ src/test/java/com/daily/TestConfiguration.java
@@ -1,25 +1,19 @@
-//package com.dlog;
-//
-//import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-//import org.springframework.batch.test.JobLauncherTestUtils;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.boot.context.properties.EnableConfigurationProperties;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.test.context.TestPropertySource;
-//
-//@Configuration
-//@EnableAutoConfiguration
-//@EnableBatchProcessing
-////@EnableConfigurationProperties
-////@TestPropertySource(properties = {"woowha.blog."})
-//public class TestConfiguration {
-//
-//    @Bean
-//    public JobLauncherTestUtils jobLauncherTestUtils() {
-//        return new JobLauncherTestUtils();
-//    }
-//
-//}
+package com.daily;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableBatchProcessing
+public class TestConfiguration {
+
+    @Bean
+    public JobLauncherTestUtils jobLauncherTestUtils() {
+        return new JobLauncherTestUtils();
+    }
+
+}
--- src/test/java/com/daily/batch/ContentsConfigurationTest.java
+++ src/test/java/com/daily/batch/ContentsConfigurationTest.java
@@ -1,50 +1,61 @@
-//package com.dlog.batch;
-//
-//import com.dlog.TestConfiguration;
-//import com.dlog.global.batch.contents.ContentsConfiguration;
-//import org.assertj.core.api.Assertions;
-//import org.junit.Test;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.runner.RunWith;
-//import org.springframework.batch.core.JobExecution;
-//import org.springframework.batch.core.JobParameters;
-//import org.springframework.batch.core.JobParametersBuilder;
-//import org.springframework.batch.core.StepExecution;
-//import org.springframework.batch.test.JobLauncherTestUtils;
-//import org.springframework.batch.test.context.SpringBatchTest;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.context.ContextConfiguration;
-//
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@SpringBatchTest
-//@RunWith(SpringRunner.class)
-//@ContextConfiguration(classes = { ContentsConfiguration.class, TestConfiguration.class })
-//@ActiveProfiles("test")
-//public class ContentsConfigurationTest {
-//
-//    @Autowired
-//    private JobLauncherTestUtils jobLauncherTestUtils;
-//
-//    @DisplayName("컨텐츠 유무 확인하기")
-//    @Test
-//    public void test_contents() throws Exception {
-//        // given
-//        JobParameters jobParameters = new JobParametersBuilder()
-//                .addString("requestDate", "2022-08-19")
-//                .toJobParameters();
-//
-//        // when
-////        jobLauncherTestUtils.setJob();
-//        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
-//
-//        // then
-//        Assertions.assertThat(jobExecution.getStepExecutions()
-//                .stream()
-//                .mapToInt(StepExecution::getWriteCount)
-//                .sum()
-//        ).isNotEqualTo(0);
-//    }
-//}
+package com.daily.batch;
+
+import com.daily.TestConfiguration;
+import com.daily.adaptor.KakaoAdaptor;
+import com.daily.adaptor.NaverNewsAdaptor;
+import com.daily.adaptor.WoowahanAdaptor;
+import com.daily.contents.repository.ContentsRepository;
+import com.daily.global.batch.contents.ContentsConfiguration;
+import com.daily.global.batch.contents.ContentsJobListener;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.batch.core.*;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+@Import({ ContentsJobListener.class })
+@ContextConfiguration(classes = { ContentsConfiguration.class, TestConfiguration.class})
+@DataJpaTest
+@ActiveProfiles("local")
+public class ContentsConfigurationTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private KakaoAdaptor kakaoAdaptor;
+
+    @Autowired
+    private WoowahanAdaptor woowahanAdaptor;
+
+    @Autowired
+    private NaverNewsAdaptor naverNewsAdaptor;
+
+    @Autowired
+    private ContentsRepository contentsRepository;
+
+    @Test
+    public void testContents() throws Exception {
+        // given
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("requestDate", "2023-03-14")
+                .toJobParameters();
+
+        // when
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+
+        // then
+        Assertions.assertEquals(BatchStatus.COMPLETED, jobExecution);
+        Assertions.assertEquals(3, contentsRepository.findAll().size());
+    }
+}
