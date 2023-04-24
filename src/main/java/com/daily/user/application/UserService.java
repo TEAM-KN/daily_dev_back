0bb28f95c52126837e5fa8aaf327a635c3a3890d
stc9606 <stc9606@naver.com>
2023-04-25 01:46:14 +0900

    .

--- src/main/java/com/daily/user/application/UserService.java
+++ src/main/java/com/daily/user/application/UserService.java
@@ -1,8 +1,25 @@
 package com.daily.user.application;
 
+import com.daily.user.domain.User;
+import com.daily.user.exception.NoSearchUserException;
+import com.daily.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User fetchUser(String id) {
+        User user = userRepository.findById(id).orElse(null);
+
+        if (user == null)
+            throw new NoSearchUserException();
+
+
+        return user;
+    }
 
 }
