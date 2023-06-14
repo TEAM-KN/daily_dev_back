c77c9ecf0f12232993cffe213101ba0ce08bfdd4
scnoh <stc9606@naver.com>
2023-06-14 23:47:34 +0900

    [1.0.2 fix] content job 테스트 코드 수정
    - content job 테스트 코드 수정

--- src/test/java/com/daily/batch/ContentConfigurationTest.java
+++ src/test/java/com/daily/batch/ContentConfigurationTest.java
@@ -1,61 +1,49 @@
-//package com.daily.batch;
-//
-//import com.daily.TestConfiguration;
-//import com.daily.adaptor.KakaoAdaptor;
-//import com.daily.adaptor.NaverNewsAdaptor;
-//import com.daily.adaptor.WoowahanAdaptor;
-//import com.daily.domain.content.repository.ContentRepository;
-//import com.daily.global.batch.contents.ContentsConfiguration;
-//import com.daily.global.batch.contents.ContentsJobListener;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.batch.core.*;
-//import org.springframework.batch.test.JobLauncherTestUtils;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.context.annotation.Import;
-//import org.springframework.test.context.ActiveProfiles;
-//
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//
-//
-//@ExtendWith(SpringExtension.class)
-//@Import({ ContentsJobListener.class })
-//@ContextConfiguration(classes = { ContentsConfiguration.class, TestConfiguration.class})
-//@DataJpaTest
-//@ActiveProfiles("local")
-//public class ContentConfigurationTest {
-//
-//    @Autowired
-//    private JobLauncherTestUtils jobLauncherTestUtils;
-//
-//    @Autowired
-//    private KakaoAdaptor kakaoAdaptor;
-//
-//    @Autowired
-//    private WoowahanAdaptor woowahanAdaptor;
-//
-//    @Autowired
-//    private NaverNewsAdaptor naverNewsAdaptor;
-//
-//    @Autowired
-//    private ContentRepository contentRepository;
-//
-//    @Test
-//    public void testContents() throws Exception {
-//        // given
-//        JobParameters jobParameters = new JobParametersBuilder()
-//                .addString("requestDate", "2023-03-14")
-//                .toJobParameters();
-//
-//        // when
-//        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
-//
-//        // then
-//        Assertions.assertEquals(BatchStatus.COMPLETED, jobExecution);
-//        Assertions.assertEquals(3, contentRepository.findAll().size());
-//    }
-//}
+package com.daily.batch;
+
+import com.daily.domain.content.repository.ContentRepository;
+import com.daily.global.batch.contents.ContentsConfiguration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@SpringBatchTest
+@ActiveProfiles("local")
+public class ContentConfigurationTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private ContentRepository contentRepository;
+
+    @Autowired
+    private ContentsConfiguration job;
+
+    @Test
+    void testContents() throws Exception {
+        // given
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("requestDate", "2023-06-14")
+                .toJobParameters();
+
+        // when
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+
+        // then
+        Assertions.assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
+        Assertions.assertEquals(3, contentRepository.findAll().size());
+    }
+}
