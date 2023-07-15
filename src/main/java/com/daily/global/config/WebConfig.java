b046c1c727bb09275f831ff65b730e191ab1fedd
s_chuli <stc9606@naver.com>
2023-07-15 16:44:05 +0900

    [v1.0.6 fix] cors 설정 수정
    - cors 설정 수정

--- src/main/java/com/daily/global/config/WebConfig.java
+++ src/main/java/com/daily/global/config/WebConfig.java
@@ -2,21 +2,25 @@ package com.daily.global.config;
 
 import com.daily.auth.application.AuthInterceptor;
 import com.daily.auth.application.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    private final List<String> corsAllowedOrigins;
     private final JwtTokenProvider jwtTokenProvider;
 
-    public WebConfig(JwtTokenProvider jwtTokenProvider) {
+    public WebConfig(@Value("${web.cors.allowed-origins}") final List<String> corsAllowedOrigins,
+                     final JwtTokenProvider jwtTokenProvider) {
+        this.corsAllowedOrigins = corsAllowedOrigins;
         this.jwtTokenProvider = jwtTokenProvider;
     }
-
-
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -26,8 +30,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        String[] patterns = corsAllowedOrigins.toArray(String[]::new);
+
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://3.239.26.220:8080")
-                .allowedMethods("OPTIONS", "POST", "GET", "PATCH", "PUT", "DELETE");
+                .allowedMethods("*")
+                .allowedOriginPatterns(patterns);
     }
 }
