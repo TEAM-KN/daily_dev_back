79f53d33ed08424fb4a5f4a989a7bd74c52f80be
scnoh <stc9606@naver.com>
2023-10-17 23:02:51 +0900

    [v1.2.0 feat] 성능 개선 - content batch 멀티 쓰레드 적용
    - 성능 개선 - content batch 멀티 쓰레드 적용

---
+++ src/main/java/com/daily/global/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package com.daily.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean
+    public ThreadPoolTaskExecutor threadPoolExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(5);
+        taskExecutor.setMaxPoolSize(30);
+        taskExecutor.setQueueCapacity(10);
+        taskExecutor.setThreadNamePrefix("async-thread-");
+        taskExecutor.initialize();
+
+        return taskExecutor;
+    }
+
+}
